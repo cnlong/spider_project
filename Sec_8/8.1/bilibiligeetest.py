ba7a6ff439aee8d5fd546660d2ca52df08bdb900
cnlong <271138425@qq.com>
2020-06-02 14:24:18 +0800

    version.0602(selenium_bilibili_code)

---
+++ Sec_8/8.1/bilibiligeetest.py
@@ -0,0 +1,153 @@
+"""
+B站滑块验证码
+https://blog.csdn.net/weixin_45042620/article/details/105905106
+因为移动轨迹的问题，滑块移动不是特别准确，还需要调整参数
+"""
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from PIL import Image
+from io import BytesIO
+import time
+import random
+from selenium.webdriver import ActionChains
+
+username = input('请输入用户名：')
+password = input('请输入密码：')
+
+class BiliGeetest():
+    """类函数"""
+    def __init__(self):
+        self.url = 'https://passport.bilibili.com/login'
+        self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait(self.browser, 20)
+        self.username = username
+        self.password = password
+
+    def get_login_btn(self):
+        """获取登录按钮"""
+        login = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-login')))
+        login.click()
+        time.sleep(3)
+        print("登录成功")
+
+    def get_screenshot(self):
+        """获取网页截图"""
+        screenshot = self.browser.get_screenshot_as_png()
+        screenshot = Image.open(BytesIO(screenshot))
+        return screenshot
+
+    def get_c_background(self):
+        """获取带缺口的图片元素"""
+        c_background = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'canvas.geetest_canvas_bg.geetest_absolute')))
+        return c_background
+
+    def get_c_slice(self):
+        """获取滑块的图片元素"""
+        c_slice = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'canvas.geetest_canvas_slice.geetest_absolute')))
+        return c_slice
+
+    def get_c_full_bg(self):
+        """获取完整的背景图片元素"""
+        c_full_bg = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'canvas.geetest_canvas_fullbg.geetest_fade.geetest_absolute')))
+        return c_full_bg
+
+    def get_background_img(self):
+        """截取带缺口的验证码图片"""
+        c_background = self.get_c_background()
+        c_slice = self.get_c_slice()
+        self.browser.execute_script("arguments[0].style=arguments[1]", c_slice, "display: none")
+        screenshot = self.get_screenshot()
+        img_size = c_background.size
+        location = c_background.location
+        top, bottom, left, right = location['y'], location['y'] + img_size['height'], location['x'], location['x'] + \
+                                   img_size['width']
+        c_background_img = screenshot.crop((left, top, right, bottom))
+        c_background_img.save('c_background_img.png')
+        self.browser.execute_script("arguments[0].style=arguments[1]", c_slice, "display: block")
+        return c_background_img
+
+    def get_full_bg_img(self):
+        """截取完整图片的验证码图片"""
+        c_full_bg = self.get_c_full_bg()
+        self.browser.execute_script("arguments[0].style=arguments[1]", c_full_bg, "display: block")
+        screenshot = self.get_screenshot()
+        img_size = c_full_bg.size
+        location = c_full_bg.location
+        top, bottom, left, right = location['y'], location['y'] + img_size['height'], location['x'], location['x'] + \
+                                   img_size['width']
+        c_full_bg_img = screenshot.crop((left, top, right, bottom))
+        c_full_bg_img.save(' c_full_bg_img.png')
+        self.browser.execute_script("arguments[0].style=arguments[1]", c_full_bg, "display: none")
+        return c_full_bg_img
+
+    def get_distance(self, image1, image2):
+        """获取图片像素差异的距离"""
+        threshold = 10
+        for x in range(image1.size[0]):
+            for y in range(image1.size[1]):
+                image1_pixel = image1.load()[x, y]
+                image2_pixel = image2.load()[x, y]
+                if not(abs(image1_pixel[0] - image2_pixel[0]) < threshold and abs(image1_pixel[1] - image2_pixel[1]) < threshold and abs(image1_pixel[2] - image2_pixel[2]) < threshold):
+                    distance = x
+                    return distance
+
+    def get_trace(self, distance):
+        """获取移动轨迹"""
+        trace = []
+        v = 0
+        mid = distance * 4/5
+        current = 0
+        while current < distance:
+            if current < mid:
+                a = 5
+            else:
+                a = -4
+            t = random.randint(3, 6)/10
+            v0 = v
+            v = v0 + a * t
+            move = v0 * t + 0.5 * a * t * t
+            if current + move >= distance:
+                move = distance - current
+            current += move
+            trace.append(round(move))
+        return trace
+
+
+    def get_slider_button(self):
+        """获取滑块移动按钮"""
+        slider = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.geetest_slider_button')))
+        return slider
+
+    def move_slier(self, slider, trace):
+        """移动滑块"""
+        ActionChains(self.browser).click_and_hold(slider).perform()
+        for x in trace:
+            ActionChains(self.browser).move_by_offset(xoffset=x, yoffset=0).perform()
+            time.sleep(0.02)
+        ActionChains(self.browser).release().perform()
+
+    def open(self):
+        """打开网页"""
+        self.browser.get(self.url)
+        username = self.browser.find_element_by_id('login-username')
+        password = self.browser.find_element_by_id('login-passwd')
+        username.send_keys(self.username)
+        password.send_keys(self.password)
+
+    def run(self):
+        """运行"""
+        self.open()
+        self.get_login_btn()
+        image1 = self.get_background_img()
+        image2 = self.get_full_bg_img()
+        distance = self.get_distance(image1, image2)
+        slider = self.get_slider_button()
+        trace = self.get_trace(distance)
+        self.move_slier(slider, trace)
+
+
+if __name__ == '__main__':
+    crack = BiliGeetest()
+    crack.run()
--- Sec_8/8.1/test8.py
+++ Sec_8/8.1/test8.py
@@ -9,6 +9,8 @@ from selenium.webdriver.common.by import By
 from PIL import Image
 from io import BytesIO
 import time
+import random
+from selenium.webdriver import ActionChains
 
 browser = webdriver.Chrome()
 url = 'https://passport.bilibili.com/login'
@@ -93,6 +95,7 @@ for i in range(fullbg_img.size[0]):
         # 判断像素的各个值颜色之差，ads()去绝对值
         # 没有差距的像素点，计算之后的值为0，小于阈值即为true,有差距的像素点，计算之后的值大于阈值，即为false
         if not(abs(bg_pixel[0]-fullbg_pixel[0]) < threshold and abs(bg_pixel[1]-fullbg_pixel[1]) < threshold and abs(bg_pixel[2]-fullbg_pixel[2]) < threshold):
+            # 一般初始小滑块都在页面最左侧，所以得出的i即为需要移动的距离
             distance = i
             print(distance)
             # break只能跳出当前的i循环，还会接着下一个i的循环，所以后续还需要加个break
@@ -104,7 +107,54 @@ for i in range(fullbg_img.size[0]):
         continue
     break
 
+# 模拟滑块移动，需要模拟人为移动的情况，一般是先加速后减速
+# 模拟移动距离的前4/5的距离为加速，后1/5的距离为减速
+# 拆分整个distance为多个小的位移距离，组成一个移动轨迹列表
+track = list()
+# 当前的位移，初始为0
+current = 0
+# 到达减速位置的距离
+mid = distance * 3/5
+# 初速度
+v0 = 5
 
+while current < distance:
+    # 当前距离小于减速位置距离，即加速，反之减速
+    if current < mid:
+        # 加速度为5
+        a = 1
+    else:
+        # 加速度为-4
+        a = -0.5
+    # # 设置每次移动之间的随机间隔时间
+    t = random.randint(3, 6)/10
+    # 位移公式，move = v0 * t + 1/2 * a * t * t
+    move = v0 * t + 1/2 * a * t * t
+    # 根据加速度计算当前速度
+    v0 = v0 + a * t
+    # 如果当前距离加上移动的距离大于目标位置时候，需要修正移动位置
+    if current + move > distance:
+        move = distance - current
+    # 计算当前移动的距离
+    current += move
+    # 移动轨迹中添加此次的移动距离
+    track.append(move)
 
+print(len(track))
+print(sum(track))
+print(track)
 
-
+# 获取移动滑块的按钮选项
+slider = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.geetest_slider_button')))
+# 调用ActionChains动作链，点住并按住当前元素
+# click_and_hold()方法鼠标悬停在滑块上，perform方法用于执行
+ActionChains(browser).click_and_hold(slider).perform()
+# 遍历轨迹列表
+for x in track:
+    # 按住移动轨迹，通过鼠标移动滑块
+    ActionChains(browser).move_by_offset(xoffset=x, yoffset=0).perform()
+    print(x)
+    # 模拟人类对准时间
+    time.sleep(0.02)
+# 释放鼠标
+ActionChains(browser).release().perform()
