b4fcc17a689604e1ce61f52f0cb5facc43c0f9f1
cnlong <271138425@qq.com>
2020-07-21 14:13:04 +0800

    version.0721(Item Pipeline)

---
+++ Sec_13/images360/images360/items.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Images360Item(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+# 定义item对象，定义数据保存的格式
+class ImageItem(scrapy.Item):
+    # MongoDB和MySQL存储的表名
+    collection = table = 'images'
+    id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    thumb = scrapy.Field()
+
+
---
+++ Sec_13/images360/images360/spiders/images.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Spider, Request
+from urllib.parse import urlencode
+import json
+from images360.items import ImageItem
+
+
+class ImagesSpider(scrapy.Spider):
+    name = 'images'
+    allowed_domains = ['image.so.com']
+    start_urls = ['http://image.so.com/']
+
+    def parse(self, response):
+        # 将json内容转换成字典
+        result = json.loads(response.text)
+        # 获取text内容中list字段中的图片并遍历
+        for image in result.get('list'):
+            # 创建item对象
+            item = ImageItem()
+            # 保存数据
+            item['id'] = image.get('id')
+            item['url'] = image.get('https://p1.ssl.qhimgs1.com/t0105567bca0b70ed01.jpg')
+            item['title'] = image.get('title')
+            item['thumb'] = image.get('https://p1.ssl.qhimgs1.com/sdr/200_200_/t0105567bca0b70ed01.jpg')
+
+    def start_requests(self):
+        # url参数
+        data = {'ch': 'photograph', 'listtype': 'new'}
+        # 基础url
+        base_url = 'https://image.so.com/zjl?'
+        # 遍历页数
+        for page in range(1, self.settings.get('MAX_PAGE') + 1):
+            # 计算sn号
+            data['sn'] = page * 30
+            # url参数编码转换
+            params = urlencode(data)
+            url = base_url + params
+            # 发起请求
+            yield Request(url, self.parse)
