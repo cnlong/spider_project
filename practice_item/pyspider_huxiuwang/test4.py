eb21222225e3d8ac7f710565284e33f1822df75e
chenlong <271138425@qq.com>
2020-10-26 15:17:05 +0800

    version.1026(虎嗅网文章信息爬取)

--- practice_item/pyspider_huxiuwang/test4.py
+++ practice_item/pyspider_huxiuwang/test4.py
@@ -2,11 +2,26 @@ import pymongo
 import pandas as pd
 import json
 import requests
+import time
+import random
 
 
 client = pymongo.MongoClient('192.168.6.160')
 db = client.huxiu
-collection = db.huxiu_new
+collection = db.huxiu_new2
+
+start_time = time.time()
+
+
+def save_to_pymongo(result):
+    # 将获取的数据转换成dataframe
+    df = pd.DataFrame(result)
+    # to_json将datafram转换成json，通过json转换成字典
+    content = json.loads(df.T.to_json()).values()
+    # print(content)
+    if collection.insert_many(content):
+        print('存储成功')
+
 
 url = 'https://article-api.huxiu.com/web/article/articleList'
 data = {
@@ -15,30 +30,41 @@ data = {
     # 'recommend_time': '1603155300'
 }
 response = requests.post(url, data=data)
-time = 0
-datas = list()
+ns = 1
 while True:
+    datas = list()
     if response.json()['success'] is True:
         recommend_time = response.json()['data']['last_dateline']
         for item in response.json()['data']['dataList']:
             datas.append(item)
+    datalist = list()
+    for item in datas:
+        dt = {
+            'title': item['title'],
+            'url': item['share_url'],
+            'author': item['user_info']['username'],
+            'write_time': item['formatDate'],
+            'comment': item['count_info']['commentnum'],
+            'favourites': item['count_info']['favtimes'],
+            'shares': item['count_info']['sharetimes']
+        }
+        datalist.append(dt)
+    print(datalist)
+    print(recommend_time)
+    if datalist == []:
+        # print(recommend_time)
+        break
+    save_to_pymongo(datalist)
     data = {
         'platform': 'www',
         'pagesize': '22',
         'recommend_time': recommend_time
     }
+    time.sleep(random.randint(0,3))
     response = requests.post(url, data=data)
-    time += 1
-    if time == 10:
+    ns += 1
+    print("第{}次开始爬取".format(ns))
+    if ns == 500:
         break
-for item in datas:
-    data = {
-        'title': item['title'],
-        'url': item['share_url'],
-        'author': item['user_info']['username'],
-        'write_time': item['formatDate'],
-        'comment': item['count_info']['commentnum'],
-        'favourites': item['count_info']['favtimes'],
-        'shares': item['count_info']['sharetimes']
-    }
-    print(data)
+
+print("耗时{}".format(time.time() - start_time))
